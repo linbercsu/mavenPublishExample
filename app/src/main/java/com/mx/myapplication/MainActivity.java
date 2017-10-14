55093bb4c8fe5257d811c6d2ac647395ec3636bc
ZhaoLinlin <linlin.zhao@zenjoy.net>
2017-10-14 15:32:21 +0800

    [ZLL] init

---
+++ app/src/main/java/com/mx/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mx.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle(R.string.test_aar_lin);
+    }
+}
